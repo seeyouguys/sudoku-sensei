93ad35c4c6dee9364820e5458d66f152aab2bc7c
kiselev_nikita <seeyouguys@mail.ru>
2022-10-11 00:59:19 +0300

    feat(NumPicker): Переделан UI

--- components/NumPicker.tsx
+++ components/NumPicker.tsx
@@ -1,9 +1,11 @@
-import React, {FC, useCallback, useMemo} from 'react';
+import React, {FC, useCallback, useMemo, useRef, useState} from 'react';
 import {
+  Animated,
   GestureResponderEvent,
   StyleSheet,
   Text,
   TouchableWithoutFeedback,
+  Vibration,
   View,
 } from 'react-native';
 import {COLORS, SIZE} from '../utils/constants';
@@ -24,23 +26,55 @@ const NumPicker: FC<Props> = ({remainingNums, setNumSelected, numSelected}) => {
   //    option и округляем. (~~ — это отбрасывание чисел после запятой)
   // 3. Получившееся число — это индекс выбранного элемента в массиве remainingNums
   const optionWidth = useMemo(() => {
-    return ~~(SIZE.MAX_WIDTH / remainingNums.length);
+    return ~~((SIZE.MAX_WIDTH * 0.95) / remainingNums.length);
   }, [remainingNums.length]);
 
-  const getChosenOption = locationX => {
-    const chosenOptionIdx = ~~(locationX / optionWidth);
+  const roundToNearestX = x => {
+    return (~~(x / optionWidth) + 0.5) * optionWidth;
+  };
+
+  const getChosenOption = x => {
+    const chosenOptionIdx = ~~(x / optionWidth);
     const newNum = remainingNums[chosenOptionIdx];
     return newNum;
   };
 
-  const onMove = (e: GestureResponderEvent) => {
-    const newNum = getChosenOption(e.nativeEvent.pageX);
-    if (newNum !== numSelected) {
-      setNumSelected(newNum);
-    }
+  const microVibration = () => {
+    Vibration.vibrate(4);
   };
 
+  const touchX = useRef(new Animated.Value(0)).current;
+  const touchLength = useRef(new Animated.Value(1)).current;
+  const touchRadius = useRef(44).current;
+
+  const animSpringTo = x =>
+    Animated.spring(touchX, {
+      toValue: x,
+      speed: 20,
+      useNativeDriver: true,
+    }).start();
+
+  const animScaleTo = s =>
+    Animated.spring(touchLength, {
+      toValue: s,
+      useNativeDriver: true,
+    }).start();
+
   const onStart = (e: GestureResponderEvent) => {
+    microVibration();
+
+    animSpringTo(e.nativeEvent.pageX);
+    animScaleTo(1.25);
+
+    const newNum = getChosenOption(e.nativeEvent.pageX);
+    setNumSelected(newNum);
+  };
+
+  const onRelease = (e: GestureResponderEvent) => {
+    const endPoint = roundToNearestX(e.nativeEvent.pageX);
+    animSpringTo(endPoint);
+    animScaleTo(1);
+
     const newNum = getChosenOption(e.nativeEvent.pageX);
     setNumSelected(newNum);
   };
@@ -50,16 +84,35 @@ const NumPicker: FC<Props> = ({remainingNums, setNumSelected, numSelected}) => {
       style={styles.container}
       onStartShouldSetResponder={() => true}
       onResponderStart={onStart}
-      onResponderMove={onMove}>
+      // onResponderMove={onMove}
+      onResponderRelease={onRelease}>
+      <Animated.View
+        style={[
+          styles.touch,
+          {
+            transform: [
+              {
+                translateX: Animated.subtract(touchX, 22),
+              },
+              {
+                scaleX: touchLength,
+              },
+            ],
+            height: touchRadius,
+            width: touchRadius,
+            borderRadius: touchRadius,
+          },
+        ]}
+      />
       {remainingNums.map((num, i) => (
         <View key={i}>
           <View
             style={[
               styles.option,
-              {
-                backgroundColor:
-                  numSelected === num ? COLORS.accent : COLORS.accent20,
-              },
+              // {
+              //   backgroundColor:
+              //     numSelected === num ? COLORS.accent : 'transparent',
+              // },
             ]}>
             <Text style={styles.optionText}>{num}</Text>
           </View>
@@ -71,14 +124,20 @@ const NumPicker: FC<Props> = ({remainingNums, setNumSelected, numSelected}) => {
 
 const styles = StyleSheet.create({
   container: {
+    // width: SIZE.MAX_WIDTH,
     width: SIZE.MAX_WIDTH * 0.95,
     marginTop: 20,
     justifyContent: 'space-between',
     flexDirection: 'row',
     flexWrap: 'wrap',
+    alignItems: 'center',
+  },
+  touch: {
+    position: 'absolute',
+    backgroundColor: COLORS.accent,
   },
   option: {
-    borderRadius: 4,
+    borderRadius: 100,
     padding: 12,
   },
   optionText: {
