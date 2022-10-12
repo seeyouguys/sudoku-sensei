a671ae97e707070d3cfeb26dccf81e16b0db97a2
kiselev_nikita <seeyouguys@mail.ru>
2022-10-12 03:14:12 +0300

    feat(AsyncStorage): Подключен AsyncStorage для хранения playerStats

--- App.tsx
+++ App.tsx
@@ -1,14 +1,74 @@
-import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React, {FC} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {useAsyncStorage} from '@react-native-async-storage/async-storage';
+import React, {FC, useState, useEffect} from 'react';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import GameScreen from './screens/GameScreen';
 import HomeScreen from './screens/HomeScreen';
 import {COLORS} from './utils/constants';
+import {Level} from './utils/SudokuSeeds.tsx';
 
 const Stack = createNativeStackNavigator();
 
+type LevelStats = {
+  bestTime: String;
+  wins: String;
+};
+
+type PlayerStats = {
+  easy: LevelStats;
+  medium: LevelStats;
+  hard: LevelStats;
+  evil: LevelStats;
+  winsToLevelUpCounter: Number;
+  maxLevel: Level;
+};
+
 const App: FC = () => {
+  const [stats, setStats] = useState<PlayerStats | null>();
+  const {getItem, setItem} = useAsyncStorage('@player_stats');
+
+  const readStatsFromStorage = async () => {
+    const loadedStats = await getItem();
+    setStats(JSON.parse(loadedStats));
+  };
+
+  const writeStatsToStorage = async (newStats: PlayerStats) => {
+    await setItem(JSON.stringify(newStats));
+    setStats(JSON.stringify(newStats));
+  };
+
+  const dummyStats: PlayerStats = {
+    easy: {
+      bestTime: Math.random().toString(36).substr(2, 5),
+      wins: ~~(Math.random() * 10),
+    },
+    medium: {
+      bestTime: Math.random().toString(36).substr(2, 5),
+      wins: ~~(Math.random() * 10),
+    },
+    hard: {
+      bestTime: Math.random().toString(36).substr(2, 5),
+      wins: ~~(Math.random() * 10),
+    },
+    evil: {
+      bestTime: Math.random().toString(36).substr(2, 5),
+      wins: ~~(Math.random() * 10),
+    },
+    winsToLevelUpCounter: ~~(Math.random() * 10),
+    maxLevel: 'medium',
+  };
+
+  useEffect(() => {
+    readStatsFromStorage();
+  }, []);
+  return (
+    <View style={{margin: 40}}>
+      <Text>Current stats: {JSON.stringify(stats, null, 2)}</Text>
+      <TouchableOpacity onPress={() => writeStatsToStorage(dummyStats)}>
+        <Text>Update stats</Text>
+      </TouchableOpacity>
+    </View>
+  );
   return (
     <View style={styles.bg}>
       <NavigationContainer>
